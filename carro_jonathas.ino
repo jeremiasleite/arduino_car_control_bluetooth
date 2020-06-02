2bc163621e39d1d38c5436168dc8a5ef4fab335d
jeremiasleite <jeremias23456@gmail.com>
2020-06-02 16:59:40 -0300

    correções de velocidade

--- carro_jonathas.ino
+++ carro_jonathas.ino
@@ -8,15 +8,17 @@ const int pinoServo = 9; //PINO DIGITAL UTILIZADO PELO SERVO
 const int pinoRX = 3; //PINO DIGITAL 2 (RX)
 const int pinoTX = 2; //PINO DIGITAL 3 (TX)
 const int pinoLed = 13; //PINO DIGITAL UTILIZADO PELO LED
-char dadoBluetooth = '0'; //VARIÁVEL QUE ARMAZENA O VALOR ENVIADO PELO BLUETOOTH
+char dadoBluetooth = '#'; //VARIÁVEL QUE ARMAZENA O VALOR ENVIADO PELO BLUETOOTH
 boolean loopLED = false; //VARIÁVEL BOOLEANA QUE FAZ O CONTROLE DE ATIVAÇÃO DO LOOP DO LED
 
 const int pinIn1 = 4;
 const int pinIn2 = 5;
 const int pinVelocidade = 11;
-int velocidade = 100;
-int loop_v = 1;
 int sentido = 1; //1-frente, 0-re
+
+boolean estaAcelerando = false;
+int velocidade = 80;
+const int velocidadeInicial = 80;
 
 Servo s; //OBJETO DO TIPO SERVO
 int pos; //POSIÇÃO DO SERVO
@@ -42,7 +44,7 @@ void setup() {
   //BLUETOOTH
   Serial.begin(9600); //INICIALIZA A SERIAL
   bluetooth.begin(9600); //INICIALIZA A SERIAL DO BLUETOOTH
-  bluetooth.print("$"); //IMPRIME O CARACTERE
+  //bluetooth.print("$"); //IMPRIME O CARACTERE
   delay(100); //INTERVALO DE 100 MILISSEGUNDOS
   pinMode(pinoLed, OUTPUT); //DEFINE O PINO COMO SAÍDA
 }
@@ -52,47 +54,54 @@ void loop() {
   if(bluetooth.available()){ //SE O BLUETOOTH ESTIVER HABILITADO, FAZ
     //Serial.println("BLUETOOTH LIGADO");
     dadoBluetooth = bluetooth.read(); 
-    Serial.println(dadoBluetooth);
+    //Serial.println(dadoBluetooth);
      
     if(dadoBluetooth == '1'){       
       //Serial.println("ESQUERDA");
-      if(sentido == 1){
-        frente(velocidade);
+      s.write(161);
+      estaAcelerando = true;
+      if(sentido == 1){        
+        frente();
       }else {
-        re(velocidade);
-      }
-      s.write(161);      
+        re();
+      }            
     }else if(dadoBluetooth == '3'){ 
       //Serial.println("DIREIRA");
-      if(sentido == 1){
-        frente(velocidade);
-      }else {
-        re(velocidade);
-      } 
+      estaAcelerando = true;
       s.write(90);      
+      if(sentido == 1){
+        frente();
+      }else {
+        re();
+      }             
     }
-    if(dadoBluetooth =='0'|| dadoBluetooth=='2'){      
+    if(dadoBluetooth =='0'|| dadoBluetooth=='2'){
+      s.write(136);      
       parar();
-      s.write(136);
+      estaAcelerando = false;
+      velocidade = velocidadeInicial;
     }
     
-    if(dadoBluetooth == '5'){
+    if(dadoBluetooth == '5'){      
       sentido = 1;
-      frente(velocidade);
-    }else if(dadoBluetooth == '7'){
-       sentido = 0;
-       re(velocidade);
-    }
-    if(dadoBluetooth == '4' || dadoBluetooth == '6'){ 
-       velocidade = 95;
-       loop_v=1;
+      frente();
+      estaAcelerando = true;
+    }else if(dadoBluetooth == '7'){      
+      sentido = 0;
+      re();
+      estaAcelerando = true;
+    }else if(dadoBluetooth == '4' || dadoBluetooth == '6'){       
        parar();
+       estaAcelerando = false;
+       velocidade = velocidadeInicial;
     }
   }
-  Serial.println(velocidade);
-  /*if(velocidade<255){    
-    velocidade = velocidade+5;
-  }*/
+  //Serial.println(velocidade);
+  if(estaAcelerando && velocidade<=255){    
+    //Serial.println(velocidade);
+    analogWrite(pinVelocidade, velocidade);
+    velocidade = velocidade + 3;
+  }
   
   if(dadoBluetooth == '9'){
     ledVermelho = !ledVermelho;
@@ -117,17 +126,14 @@ void loop() {
   delay(50);
 }
 
-void frente(int velocidade) {
-  int velocidade = 100;
-  analogWrite(pinVelocidade, velocidade);  
+void frente() {
   digitalWrite(pinIn1, HIGH);
-  digitalWrite(pinIn2, LOW);
+  digitalWrite(pinIn2, LOW);   
 }
 
-void re(int velocidade) {
-  analogWrite(pinVelocidade, velocidade);
+void re() {
   digitalWrite(pinIn1, LOW);
-  digitalWrite(pinIn2, HIGH);
+  digitalWrite(pinIn2, HIGH);  
 }
 
 void parar() {
